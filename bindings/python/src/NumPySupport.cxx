0d11d0a5e464dde5d51730f7a39207e9c24611ee
Thomas Helfer <thomas.helfer@cea.fr>
2025-04-07 13:28:54 +0200

    cmake updates

--- bindings/python/src/NumPySupport.cxx
+++ bindings/python/src/NumPySupport.cxx
@@ -13,21 +13,16 @@
  */
 
 #include <numpy/ndarrayobject.h>
+#include <boost/python/numpy.hpp>
 #include "MGIS/Raise.hxx"
 #include "MGIS/Python/NumPySupport.hxx"
 
 namespace mgis::python {
 
-#if PY_MAJOR_VERSION == 2
-  static void wrapInitializeNumPy() { import_array(); }
-#else
-  static void* wrapInitializeNumPy() {
-    import_array();
-    return nullptr;
-  }
-#endif
-
-  void initializeNumPy() { wrapInitializeNumPy(); }  // end of initializeNumPy
+  void initializeNumPy() { 
+    Py_Initialize();
+    boost::python::numpy::initialize();
+ }  // end of initializeNumPy
 
   boost::python::object wrapInNumPyArray(std::span<double>& v) {
     npy_intp dims[1] = {static_cast<npy_intp>(v.size())};
