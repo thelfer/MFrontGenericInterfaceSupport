a6b88bfdd9cd9697ff9b94de352fe3c4eac170ff
Thomas Helfer <thomas.helfer@cea.fr>
2025-09-10 12:04:55 +0200

    wip

--- src/MechanicalPostprocessings.cxx
+++ src/MechanicalPostprocessings.cxx
@@ -5,6 +5,8 @@
  * \date   01/09/2025
  */
 
+#ifdef MGIS_HAVE_TFEL
+
 #include <optional>
 #include "MGIS/AbstractErrorHandler.hxx"
 #include "MGIS/Behaviour/Hypothesis.hxx"
@@ -17,36 +19,6 @@
 namespace mgis::function {
 
   namespace internals {
-
-    template <bool is_mutable>
-    static std::optional<FunctionView<BasicLinearSpace, {}, is_mutable>>
-    view_impl(
-        AbstractErrorHandler& ctx,
-        typename FunctionView<BasicLinearSpace, {}, is_mutable>::ExternalData
-            values,
-        const FunctionDataLayout<{}>& l) {
-      using FunctionView = FunctionView<BasicLinearSpace, {}, is_mutable>;
-      const auto n = [&values, &l]() -> size_type {
-        auto dv =
-            std::div(static_cast<long long>(values.size()), l.getDataStride());
-        return static_cast<size_type>(dv.quot);
-      }();
-      if (n == 0) {
-        return ctx.registerErrorMessage("invalid values size");
-      }
-      const auto space = BasicLinearSpace{n};
-      if (!FunctionView::checkPreconditions(ctx, space, values, l)) {
-        return {};
-      }
-      return FunctionView{space, values, l};
-    }  // end of view_impl
-
-    static std::optional<FunctionView<BasicLinearSpace, {}, false>> view(
-        AbstractErrorHandler& ctx,
-        std::span<const real> values,
-        const FunctionDataLayout<{}>& l) {
-      return internals::view_impl<false>(ctx, values, l);
-    }  // end of view
 
     template <EvaluatorConcept EvaluatorType>
     std::optional<Function<BasicLinearSpace>> evaluate(
@@ -65,52 +37,41 @@ namespace mgis::function {
 
     template <EvaluatorModifierConcept ModifierType>
     std::optional<Function<BasicLinearSpace>>
-    evaluateUnaryOperationOnSymmetricTensor(AbstractErrorHandler& ctx,
-                                            std::span<const real> values,
-                                            const FunctionDataLayout<{}>& l,
-                                            const ModifierType& op) {
-      const auto s = view(ctx, values, l);
-      if (isInvalid(s)) {
-        return {};
-      }
-      if (l.getNumberOfComponents() == 3) {
-        return internals::evaluate(ctx, *s | as_stensor<1> | op);
-      } else if (l.getNumberOfComponents() == 4) {
-        return internals::evaluate(ctx, *s | as_stensor<2> | op);
-      } else if (l.getNumberOfComponents() == 6) {
-        return internals::evaluate(ctx, *s | as_stensor<3> | op);
+    evaluateUnaryOperationOnSymmetricTensor(
+        AbstractErrorHandler& ctx,
+        const FunctionView<BasicLinearSpace, {}, false>& f,
+        const ModifierType& op) {
+      if (getNumberOfComponents(f) == 3) {
+        return internals::evaluate(ctx, f | as_stensor<1> | op);
+      } else if (getNumberOfComponents(f) == 4) {
+        return internals::evaluate(ctx, f | as_stensor<2> | op);
+      } else if (getNumberOfComponents(f) == 6) {
+        return internals::evaluate(ctx, f | as_stensor<3> | op);
       }
       return ctx.registerErrorMessage("invalid number of components");
     }  // end of evaluateUnaryOperationOnSymmetricTensor
 
   }  // end of namespace internals
 
-#ifdef MGIS_HAVE_TFEL
-
   std::optional<Function<BasicLinearSpace>> computeTrace(
       AbstractErrorHandler& ctx,
-      std::span<const real> values,
-      const FunctionDataLayout<{}>& l) {
-    return internals::evaluateUnaryOperationOnSymmetricTensor(ctx, values, l,
-                                                              trace);
+      const FunctionView<BasicLinearSpace, {}, false>& f) {
+    return internals::evaluateUnaryOperationOnSymmetricTensor(ctx, f, trace);
   }  // end of computeTrace
 
   std::optional<Function<BasicLinearSpace>> computeMisesStress(
       AbstractErrorHandler& ctx,
-      std::span<const real> values,
-      const FunctionDataLayout<{}>& l) {
-    return internals::evaluateUnaryOperationOnSymmetricTensor(ctx, values, l,
-                                                              vmis);
+      const FunctionView<BasicLinearSpace, {}, false>& f) {
+    return internals::evaluateUnaryOperationOnSymmetricTensor(ctx, f, vmis);
   }  // end of computeMisesStress
 
   std::optional<Function<BasicLinearSpace>> computeHydrostraticPressure(
       AbstractErrorHandler& ctx,
-      std::span<const real> values,
-      const FunctionDataLayout<{}>& l) {
+      const FunctionView<BasicLinearSpace, {}, false>& f) {
     return internals::evaluateUnaryOperationOnSymmetricTensor(
-        ctx, values, l, hydrostatic_stress);
+        ctx, f, hydrostatic_stress);
   }  // end of computeHydrostraticPressure
 
-#endif /* MGIS_HAVE_TFEL */
-
 }  // end of namespace mgis::function
+
+#endif /* MGIS_HAVE_TFEL */
