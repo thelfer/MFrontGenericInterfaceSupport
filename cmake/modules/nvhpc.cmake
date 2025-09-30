3ea8366c195ee9f7820fd94d911f989b81a761c9
Thomas Helfer <thomas.helfer@cea.fr>
2025-09-30 13:52:06 +0200

    add support for -stdpar (requires unified memory support)

--- cmake/modules/nvhpc.cmake
+++ cmake/modules/nvhpc.cmake
@@ -1,6 +1,6 @@
 if((NOT CMAKE_BUILD_TYPE) OR (CMAKE_BUILD_TYPE STREQUAL "Release"))
   set(OPTIMISATION_FLAGS "-O2 -DNDEBUG ${OPTIMISATION_FLAGS}")
-  tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS_MARCH "fast")
+  mgis_enable_cxx_compiler_flag(OPTIMISATION_FLAGS_MARCH "fast")
 endif((NOT CMAKE_BUILD_TYPE) OR (CMAKE_BUILD_TYPE STREQUAL "Release"))
 
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
@@ -10,7 +10,12 @@ endif(CMAKE_BUILD_TYPE STREQUAL "Debug")
 add_definitions("-Ktrap=fp -Kieee -noswitcherror -Wno-long-long -Wall -Wshadow -Wextra -Wno-unused-parameter -Werror --display_error_number --diag_suppress1 --diag_suppress185 --diag_suppress941 -Wunused-variable --diag_suppress177 --diag_suppress191 --diag_suppress3312 --diag_suppress2811")
 
 if(enable-fast-math)
-  tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS  "ffinite-math-only")
+  mgis_enable_cxx_compiler_flag(OPTIMISATION_FLAGS  "ffinite-math-only")
 else(enable-fast-math)
-  tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS2 "ffinite-math-only")
+  mgis_enable_cxx_compiler_flag(OPTIMISATION_FLAGS2 "ffinite-math-only")
 endif(enable-fast-math)
+
+if(enable-gpu-offloading)
+  add_compile_options(-stdpar)
+  set(MGIS_ADDITIONAL_LINK_FLAGS "-stdpar")
+endif(enable-gpu-offloading)
