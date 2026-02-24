4bd091535a5e7b1ddf05d336d3bc2ac9b28a4f4a
Thomas Helfer <thomas.helfer@cea.fr>
2026-02-24 14:52:54 +0100

    Add simple support for log messages. Start working the a failure handler

--- include/MGIS/Context.hxx
+++ include/MGIS/Context.hxx
@@ -7,8 +7,12 @@
 #ifndef LIB_MGIS_CONTEXT_HXX
 #define LIB_MGIS_CONTEXT_HXX 1
 
+#include <memory>
+#include <variant>
 #include <ostream>
 #include "MGIS/Config.hxx"
+#include "MGIS/Raise.hxx"
+#include "MGIS/LogStream.hxx"
 #include "MGIS/VerbosityLevel.hxx"
 #include "MGIS/ErrorBacktrace.hxx"
 
@@ -33,9 +37,32 @@ namespace mgis {
    * to debug a specific rm.
    *
    * The default logging stream is the one returned by the
-   * `mgis::getLogStream` free function.
+   * `mgis::getDefaultLogStream` free function.
    */
   struct MGIS_EXPORT Context final : public ErrorBacktrace {
+    enum FailureHandlerPolicy { ABORT, RAISE };
+    template <FailureHandlerPolicy policy>
+    struct FailureHandler {
+      FailureHandler(Context &c) : ctx(c) {}
+      template <typename T>
+      constexpr T &&operator()(std::optional<T> &&v) const {
+        if (isInvalid(v)) {
+          raise(this->ctx.getErrorMessage());
+        }
+        return std::move(*v);
+      }
+      template <typename T>
+      constexpr T &operator()(OptionalReference<T> &&v) const {
+        if (isInvalid(v)) {
+          raise(this->ctx.getErrorMessage());
+        }
+        return *v;
+      }
+
+     private:
+      //! \brief reference to the context that created the failure handler
+      Context &ctx;
+    };
     /*!
      * \brief default constructor
      *
@@ -55,6 +82,77 @@ namespace mgis {
      * \param[in] l: the new verbose level
      */
     void setVerbosityLevel(const VerbosityLevel) noexcept;
+    /*!
+     * \return a failure handler
+     * \param[in] policy: policy used to treat a failure
+     * \note the context must outlive the failure hander
+     */
+    template <FailureHandlerPolicy policy = FailureHandlerPolicy::RAISE>
+    constexpr FailureHandler<policy> getFailureHandler() {
+      return {*this};
+    }
+    /*!
+     * \brief set the current log stream.
+     * \param[in] s: log stream
+     * \note the user is responsible for ensuring that the given object is alive
+     */
+    void setLogStream(std::ostream &) noexcept;
+    /*!
+     * \brief set the current log stream.
+     * \param[in] s: log stream
+     * \note if an empty shared pointer is given, the log stream is reset to the
+     * default one, i.e. the one returned by the `mgis::getDefaultLogStream`
+     * free function.
+     */
+    void setLogStream(std::shared_ptr<std::ostream>) noexcept;
+    //! \brief reset the default log stream
+    void resetLogStream() noexcept;
+    /*!
+     *  \brief disable the default log stream
+     *
+     * \note logging is disable by creating a no-op output stream
+     */
+    void disableLogStream() noexcept;
+    /*!
+     * \return the current log stream
+     *
+     * \note if no log stream is set, the default one is returned. See
+     * `getDefaultLogStream` for details.
+     */
+    [[nodiscard]] std::ostream &log() noexcept;
+    /*!
+     * \brief display the given arguments in the log stream if the current
+     * verbosity level (as returned by the `getVerbosityLevel` method) is
+     * greater than a minimal one.
+     *
+     * \tparam Args: types of the arguments
+     * \return the current log stream
+     * \param[in] l: minimal verbosity level
+     * \param[in] args: streamed object
+     * \note note nothing is displayed if the current verbositiy
+     * level is below the first argument
+     */
+    template <typename... Args>
+    std::ostream &log(const VerbosityLevel, Args &&...) noexcept;
+    /*!
+     * \brief a simple wrapper around the `log` method to print a warning
+     *
+     * \tparam Args: types of the arguments
+     * \param[in] args: streamed object
+     */
+    template <typename... Args>
+    void warning(Args &&...) noexcept;
+    /*!
+     * \brief a simple wrapper around the `log` method which sets the minimun
+     * verbosity level to `verboseDebug`
+     *
+     * \tparam Args: types of the arguments
+     * \param[in] args: streamed object
+     * \note note nothing is displayed if the current verbositiy level is below
+     * `verboseDebug`
+     */
+    template <typename... Args>
+    void debug(Args &&...) noexcept;
     //! \brief destructor
     ~Context() noexcept override;
 
@@ -64,13 +162,19 @@ namespace mgis {
     Context(const Context &) = delete;
     Context &operator=(Context &&) = delete;
     Context &operator=(const Context &) = delete;
+    //! \brief current log stream
+    std::variant<std::monostate, std::ostream *, std::shared_ptr<std::ostream>>
+        log_stream;
     /*!
-     * \brief local level of verbosity, initialize by the global option
-     * returned by the `getVerbosityLevel` function
+     * \brief local level of verbosity, initialize by the
+     * global option returned by the `getVerbosityLevel`
+     * function
      */
     VerbosityLevel verbosity;
   };  // end of class Context
 
 }  // end of namespace mgis
+
+#include "MGIS/Context.ixx"
 
 #endif /* LIB_MGIS_CONTEXT_HXX */
--- src/Context.cxx
+++ src/Context.cxx
@@ -4,6 +4,8 @@
  * \date   08/02/2023
  */
 
+#include <fstream>
+#include "MGIS/LogStream.hxx"
 #include "MGIS/Context.hxx"
 
 namespace mgis {
@@ -22,6 +24,57 @@ namespace mgis {
   void Context::setVerbosityLevel(const VerbosityLevel l) noexcept {
     this->verbosity = l;
   }  // end of setVerbosityLevel
+
+  void Context::setLogStream(std::ostream& s) noexcept {
+    this->log_stream = &s;
+  }  // end of setLogStream
+
+  void Context::setLogStream(std::shared_ptr<std::ostream> s) noexcept {
+    if (s.get() == nullptr) {
+      this->resetLogStream();
+      return;
+    }
+    this->log_stream = s;
+  }  // end of setLogStream
+
+  void Context::resetLogStream() noexcept{
+    this->log_stream = std::monostate{};
+  }
+
+  void Context::disableLogStream() noexcept {
+    /*!
+     * \brief a buffer which allows to create no-op output streams, i.e. streams
+     * that does not nothing.
+     *
+     * see
+     * https://stackoverflow.com/questions/11826554/standard-no-op-output-stream
+     * for details
+     */
+    struct NullBuffer : public std::streambuf {
+      int overflow(int c) override { return c; }
+    };
+    static NullBuffer null_buffer;
+    static std::ostream null_stream(&null_buffer);
+    this->setLogStream(null_stream);
+  }
+
+  std::ostream &Context::log() noexcept {
+    if (std::holds_alternative<std::ostream *>(this->log_stream)) {
+      auto *const ptr = std::get<std::ostream *>(this->log_stream);
+      if (ptr == nullptr) {
+        return ::mgis::getDefaultLogStream();
+      }
+      return *ptr;
+    } else if (std::holds_alternative<std::shared_ptr<std::ostream>>(
+                   this->log_stream)) {
+      auto ptr = std::get<std::shared_ptr<std::ostream>>(this->log_stream);
+      if (ptr.get() == nullptr) {
+        return ::mgis::getDefaultLogStream();
+      }
+      return *ptr;
+    }
+    return ::mgis::getDefaultLogStream();
+  } // end of log
 
   Context::~Context() noexcept = default;
 
